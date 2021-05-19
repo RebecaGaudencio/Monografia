9a05b8e1b2ab5915464e2c35f29733f2dd2e8d26
rebgau <rebeca.gaudencio.ismart@gmail.com>
2021-05-19 15:15:47 -0300

    Instalacao dos pacotes concluida

--- build/code/master_build.R
+++ build/code/master_build.R
@@ -4,15 +4,25 @@
 #####                                                          #####
 ####################################################################
 
+# Pacotes necessarios
 install.packages("usethis")
 install.packages("PNADcIBGE")
-library(PNADcIBGE)
+install.packages("tidyverse")
+install.packages("magrittr")
 
-# Importação Online
+
+# Chamando as bibliotecas
+library(PNADcIBGE)
+library(conflicted)
+library(tidyverse)
+library(magrittr)
+
+# Importacao dos dados e leitura da PNADc 
 help("get_pnadc")
 
 input_path <- file.path("C:/Users/rebec/Documents/GitHub/Monografia/build/input")
 setwd(input_path)
+
 lista_PNAD = list.files(pattern = "PNADC_012012.txt")
 chave_input = list.files(pattern = "input_PNADC_trimestral.sas")
 
@@ -20,8 +30,6 @@ chave_input = list.files(pattern = "input_PNADC_trimestral.sas")
 pnadc_df = read_pnadc(microdata=lista_PNAD, input_txt = chave_input)
 
 
-input_path <- pnadc_example(path="PNADC_012012.txt")
-data_path <- pnadc_example(path="exampledata.txt")
-pnadc.df <- read_pnadc(microdata=data_path, input_txt=input_path, vars=c("VD4001","VD4002"))
+pnadc_df %>% sum(V1028)
 
 
