00e3ffed0a5cd3d20b2cc8612cc8d96844db1bcf
rebgau <rebeca.gaudencio.ismart@gmail.com>
2021-05-24 12:14:24 -0300

    Criacao das variaveis macro

--- build/code/master_build.R
+++ build/code/master_build.R
@@ -32,4 +32,42 @@ pnadc_df = read_pnadc(microdata=lista_PNAD, input_txt = chave_input)
 
 pnadc_df %>% sum(V1028)
 
+populacao <- pnadc_df %>%
+  select(UF, Trimestre, V1028) %>%
+  group_by(UF,Trimestre) %>%
+  mutate(aux = sum(V1028)) %>%
+  summarise (populacao = mean(aux))
+
+ocupacao <- pnadc_df %>%
+  select(UF, Trimestre, V1028, VD4002) %>%
+  dplyr::filter(VD4002 == 1) %>%
+  group_by(UF,Trimestre) %>%
+  mutate(aux = sum(V1028)) %>%
+  summarise (ocupacao = mean(aux))
+
+
+PEA <- pnadc_df %>%
+  select(UF, Trimestre, V1028, VD4002) %>%
+  dplyr::filter(VD4002 == 1 |VD4002 == 2) %>%
+  group_by(UF,Trimestre) %>%
+  mutate(aux = sum(V1028)) %>%
+  summarise (PEA = mean(aux))
+
+
+PIA <- pnadc_df %>%
+  select(UF, Trimestre, V1028, V2009) %>%
+  dplyr::filter(V2009 >= 14) %>%
+  group_by(UF,Trimestre) %>%
+  mutate(aux = sum(V1028)) %>%
+  summarise (PIA = mean(aux))
+
+informais <- pnadc_df %>%
+  select(UF, Trimestre, V1028, VD4009, VD4012) %>%
+  dplyr::filter(VD4009 == 2 |VD4009 == 4| VD4009 == 6| (VD4009 == 8 & VD4012 == 2) | (VD4009 == 9 & VD4012 == 2) | VD4009 == 10) %>%
+  group_by(UF,Trimestre) %>%
+  mutate(aux = sum(V1028)) %>%
+  summarise (informais = mean(aux))
+
+
+
 
