cd86cb6a0adb80b8c7f8f6ac2abd9cc68491e16d
rebgau <rebeca.gaudencio.ismart@gmail.com>
2021-05-25 10:09:28 -0300

    Atualiza variaveis do mercado de trabalho

--- build/code/master_build.R
+++ build/code/master_build.R
@@ -1,6 +1,6 @@
 ####################################################################
 #####                                                          #####
-##                           Monografia                           ##
+##                           MONOGRAFIA                           ##
 #####                                                          #####
 ####################################################################
 
@@ -40,11 +40,11 @@ populacao <- pnadc_df %>%
 
 ############################################################
 #                                                          #   
-##                Variaveis DO Mercado De Trabalho        ## 
+##                VARIAVEIS DO MERCADO DE TRABALHO        ## 
 #                                                          #
 ############################################################
 
-# Força de Trabalho por estado #
+# 1. Força de Trabalho por estado #
 workforce <- pnadc_df %>%
   select(UF, Trimestre, V1028, VD4001) %>%
   dplyr::filter(VD4001 == 1) %>%
@@ -53,7 +53,7 @@ workforce <- pnadc_df %>%
   summarise (workforce = mean(aux))
 
 
-# Ocupacao por estado # 
+# 2. Ocupacao e Desocupacao  por estado # 
 
 ocupacao <- pnadc_df %>%
   select(UF, Trimestre, V1028, VD4002) %>%
@@ -62,8 +62,15 @@ ocupacao <- pnadc_df %>%
   mutate(aux = sum(V1028)) %>%
   summarise (ocupacao = mean(aux))
 
+desocupados <- pnadc_df %>%
+  select(UF, Trimestre, V1028, VD4002) %>%
+  dplyr::filter(VD4002 == 2) %>%
+  group_by(UF,Trimestre) %>%
+  mutate(aux = sum(V1028)) %>%
+  summarise (desocupados = mean(aux))
+
 ##########################################################
-##              Ocupacao por Faixa Etaria               ##
+##       Ocupacao e Desocupacao por Faixa Etaria        ##
 #                                                        #
 #        1. Adolescentes --> 14 a 19 anos                #
 #        2. Jovens --> 20 a 24 anos                      #
@@ -105,8 +112,41 @@ ocup4 <- pnadc_df %>%
   mutate(aux = sum(V1028)) %>%
   summarise (ocup4 = mean(aux))
 
+
+desocup1 <- pnadc_df %>%
+  select(UF, Trimestre, V1028, VD4002, V2009) %>%
+  dplyr::filter(VD4002 == 2 & V2009 >= 14 & V2009 <= 19) %>%
+  group_by(UF,Trimestre) %>%
+  mutate(aux = sum(V1028)) %>%
+  summarise (desocup1 = mean(aux))
+
+
+
+desocup2 <- pnadc_df %>%
+  select(UF, Trimestre, V1028, VD4002, V2009) %>%
+  dplyr::filter(VD4002 == 2 & V2009 >= 20 & V2009 <= 24) %>%
+  group_by(UF,Trimestre) %>%
+  mutate(aux = sum(V1028)) %>%
+  summarise (desocup2 = mean(aux))
+
+
+desocup3 <- pnadc_df %>%
+  select(UF, Trimestre, V1028, VD4002, V2009) %>%
+  dplyr::filter(VD4002 == 2 & V2009 >= 25 & V2009 <= 59) %>%
+  group_by(UF,Trimestre) %>%
+  mutate(aux = sum(V1028)) %>%
+  summarise (desocup3 = mean(aux))
+
+
+desocup4 <- pnadc_df %>%
+  select(UF, Trimestre, V1028, VD4002, V2009) %>%
+  dplyr::filter(VD4002 == 2 & V2009 >= 60) %>%
+  group_by(UF,Trimestre) %>%
+  mutate(aux = sum(V1028)) %>%
+  summarise (desocup4 = mean(aux))
+
 ##########################################################
-##             Ocupacao por Escolaridade                ##
+#        Ocupacao e Desocupacao por Escolaridade        ##
 #                                                        #
 #        1. Sem instrução                                #
 #        2. Fundamental incompleto ou eq                 #
@@ -174,108 +214,6 @@ ocupesco7 <-  pnadc_df %>%
   mutate(aux = sum(V1028)) %>%
   summarise (ocupesco7 = mean(aux))
 
-
-# Pop. Economicamente Ativa por estado # 
-
-PEA <- pnadc_df %>%
-  select(UF, Trimestre, V1028, VD4002) %>%
-  dplyr::filter(VD4002 == 1 |VD4002 == 2) %>%
-  group_by(UF,Trimestre) %>%
-  mutate(aux = sum(V1028)) %>%
-  summarise (PEA = mean(aux))
-
-# Pop. em Ativa (>14 anos) por estado # 
-
-PIA <- pnadc_df %>%
-  select(UF, Trimestre, V1028, V2009) %>%
-  dplyr::filter(V2009 >= 14) %>%
-  group_by(UF,Trimestre) %>%
-  mutate(aux = sum(V1028)) %>%
-  summarise (PIA = mean(aux))
-
-
-# No. de informais por estado #
-
-informais <- pnadc_df %>%
-  select(UF, Trimestre, V1028, VD4009, VD4012) %>%
-  dplyr::filter(VD4009 == 2 |VD4009 == 4| VD4009 == 6| (VD4009 == 8 & VD4012 == 2) | (VD4009 == 9 & VD4012 == 2) | VD4009 == 10) %>%
-  group_by(UF,Trimestre) %>%
-  mutate(aux = sum(V1028)) %>%
-  summarise (informais = mean(aux))
-
-# No. de formais por estado #
-
-formais <- pnadc_df %>%
-  select(UF, Trimestre, V1028, VD4009, VD4012) %>%
-  dplyr::filter(VD4009 == 1 |VD4009 == 3| VD4009 == 5| VD4009 == 7|(VD4009 == 8 & VD4012 == 1) | (VD4009 == 9 & VD4012 == 2)) %>%
-  group_by(UF,Trimestre) %>%
-  mutate(aux = sum(V1028)) %>%
-  summarise (formais = mean(aux))
-
-# No. de desocupados por estado #
-
-desocupados <- pnadc_df %>%
-  select(UF, Trimestre, V1028, VD4002) %>%
-  dplyr::filter(VD4002 == 2) %>%
-  group_by(UF,Trimestre) %>%
-  mutate(aux = sum(V1028)) %>%
-  summarise (desocupados = mean(aux))
-
-##########################################################
-##            Desocupados por Faixa Etaria              ##
-#                                                        #
-#        1. Adolescentes --> 14 a 19 anos                #
-#        2. Jovens --> 20 a 24 anos                      #
-#        3. Adultos --> 25 a 59 anos                     #
-#        4. Idosos --> 60+                               #
-##                                                      ## 
-##########################################################
-
-desocup1 <- pnadc_df %>%
-  select(UF, Trimestre, V1028, VD4002, V2009) %>%
-  dplyr::filter(VD4002 == 2 & V2009 >= 14 & V2009 <= 19) %>%
-  group_by(UF,Trimestre) %>%
-  mutate(aux = sum(V1028)) %>%
-  summarise (desocup1 = mean(aux))
-
-
-
-desocup2 <- pnadc_df %>%
-  select(UF, Trimestre, V1028, VD4002, V2009) %>%
-  dplyr::filter(VD4002 == 2 & V2009 >= 20 & V2009 <= 24) %>%
-  group_by(UF,Trimestre) %>%
-  mutate(aux = sum(V1028)) %>%
-  summarise (desocup2 = mean(aux))
-
-
-desocup3 <- pnadc_df %>%
-  select(UF, Trimestre, V1028, VD4002, V2009) %>%
-  dplyr::filter(VD4002 == 2 & V2009 >= 25 & V2009 <= 59) %>%
-  group_by(UF,Trimestre) %>%
-  mutate(aux = sum(V1028)) %>%
-  summarise (desocup3 = mean(aux))
-
-
-desocup4 <- pnadc_df %>%
-  select(UF, Trimestre, V1028, VD4002, V2009) %>%
-  dplyr::filter(VD4002 == 2 & V2009 >= 60) %>%
-  group_by(UF,Trimestre) %>%
-  mutate(aux = sum(V1028)) %>%
-  summarise (desocup4 = mean(aux))
-
-##########################################################
-##            Desocupacao por Escolaridade              ##
-#                                                        #
-#        1. Sem instrução                                #
-#        2. Fundamental incompleto ou eq                 #
-#        3. Fundamental completo                         #
-#        4. Medio incompleto                             #
-#        5. Medio completo                               #
-#        6. Superior incompleto                          #
-#        7. Superior completo                            #
-##                                                      ## 
-##########################################################
-
 desocupesco1 <-  pnadc_df %>%
   select(UF, Trimestre, V1028, VD4002, VD3004) %>%
   dplyr::filter(VD4002 == 2 & VD3004 == 1) %>%
@@ -332,8 +270,44 @@ desocupesco7 <-  pnadc_df %>%
   summarise (desocupesco7 = mean(aux))
 
 
+# 3. Pop. Economicamente Ativa por estado # 
 
-# No. de desalentados por estado #
+PEA <- pnadc_df %>%
+  select(UF, Trimestre, V1028, VD4002) %>%
+  dplyr::filter(VD4002 == 1 |VD4002 == 2) %>%
+  group_by(UF,Trimestre) %>%
+  mutate(aux = sum(V1028)) %>%
+  summarise (PEA = mean(aux))
+
+# 4. Pop. em Ativa (>14 anos) por estado # 
+
+PIA <- pnadc_df %>%
+  select(UF, Trimestre, V1028, V2009) %>%
+  dplyr::filter(V2009 >= 14) %>%
+  group_by(UF,Trimestre) %>%
+  mutate(aux = sum(V1028)) %>%
+  summarise (PIA = mean(aux))
+
+
+# 5. No. de informais por estado #
+
+informais <- pnadc_df %>%
+  select(UF, Trimestre, V1028, VD4009, VD4012) %>%
+  dplyr::filter(VD4009 == 2 |VD4009 == 4| VD4009 == 6| (VD4009 == 8 & VD4012 == 2) | (VD4009 == 9 & VD4012 == 2) | VD4009 == 10) %>%
+  group_by(UF,Trimestre) %>%
+  mutate(aux = sum(V1028)) %>%
+  summarise (informais = mean(aux))
+
+# 6. No. de formais por estado #
+
+formais <- pnadc_df %>%
+  select(UF, Trimestre, V1028, VD4009, VD4012) %>%
+  dplyr::filter(VD4009 == 1 |VD4009 == 3| VD4009 == 5| VD4009 == 7|(VD4009 == 8 & VD4012 == 1) | (VD4009 == 9 & VD4012 == 2)) %>%
+  group_by(UF,Trimestre) %>%
+  mutate(aux = sum(V1028)) %>%
+  summarise (formais = mean(aux))
+
+# 7. No. de desalentados por estado #
 
 desalentados <- pnadc_df %>%
   select(UF, Trimestre, V1028, VD4005) %>%
@@ -365,8 +339,10 @@ nemnem <- pnadc_df %>%
 
 ####################################################
 #                                                  #
-#            1. Taxa de Desemprego                 #
+#                       TAXAS                      #
 #                                                  #
 ####################################################
 
-Txdesemprego <- (desocupados/workforce)*100
+# 1. Taxa de Desemprego
+
+Txdesemprego <- (desocupados[,3]/workforce[,3])
